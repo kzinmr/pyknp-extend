302b3992ac0b181c4320943482a4db6fa931ce18
Kazuki Inamura <kzinmr109@gmail.com>
2018-01-31 16:44:09 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-__author__ = 'John Richardson'
-__email__ = 'john@nlp.ist.i.kyoto-u.ac.jp'
+__author__ = 'Kazuki Inamura'
+__email__ = 'kzinmr109@gmail.com'
 __copyright__ = ''
 __license__ = 'See COPYING'
 
 import os
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 
 version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
 with open(version_file) as fh:
@@ -14,15 +14,14 @@ with open(version_file) as fh:
 __version__ = pyknp_version
 
 setup(
-    name='pyknp',
+    name='pyknp-extend',
     version=pyknp_version,
     maintainer=__author__,
     maintainer_email=__email__,
     author=__author__,
     author_email=__email__,
-    description='Python module for JUMAN/KNP.',
+    description='Feature extension of pyknp-0.3.',
     license=__license__,
-    url='https://bitbucket.org/ku_nlp/pyknp',
-    scripts=['pyknp/scripts/knp-drawtree', ],
+    url='https://github.com/kzinmr/pyknp-extend',
     packages=find_packages(),
 )
